28142ccde9642c1f62c670aa9dba89edf9d63d50
Rushi M. Ganmukhi <rushimg@rosetta14.csail.mit.edu>
2013-12-12 18:02:56 -0500

    added stuff for comaring comments rather than code snippets

--- python_stuff/tcode_2_vecs.py
+++ python_stuff/tcode_2_vecs.py
@@ -3,8 +3,8 @@ import fnmatch
 import xml.etree.ElementTree as ET
 
 MATCH = "doc_*"
-DIR_NAME = "../code_corpus/train/train_code_w_doc"
-TRAIN = "../train_test/train.dat"
+DIR_NAME = "../code_corpus/test/test_code_w_doc"
+TRAIN = "../train_test/test.dat"
 WORDS = "../train_test/words"
 CORRECT = ['0']
 def process_files():
@@ -13,19 +13,22 @@ def process_files():
         for root, dirnames, filenames in os.walk(DIR_NAME):
                 for filename in fnmatch.filter(filenames, MATCH):
                         matches.append(os.path.join(root, filename))
-	words = set()
+	words = get_all_words_file()
+	#if "htmldocument" in words:
+		#print "asdas"
 	# first iteration through matches to get all words
 	for match in matches:
 		words_per_file[match] = get_all_words(match)
-		words = words.union(words_per_file[match])
+		#words = words.union(words_per_file[match])
 	#print words
+
 	train_f = open(TRAIN, 'w')
-	# second iteration through matches to get all word counts
 	pattern = MATCH.replace('*','')
+	# second iteration through matches to get all word counts
 	for match in matches:
 		#print match.replace(DIR_NAME+'/code_','')
 		#print CORRECT
-		if match.replace(DIR_NAME+'/'+pattern,'') in CORRECT:
+		if match.replace(DIR_NAME+'/' + pattern,'') in CORRECT:
 			#print words_per_file[match]
 			train_f.write('1')
 		else:
@@ -40,13 +43,14 @@ def get_word_counts(file_words, all_words):
 	word_count_dict = dict()
 	# initialize word dictionary
 	word_2_num_trans = list()
-	words_f = open(WORDS, 'w')
+	#words_f = open(WORDS, 'w')
 	for word in all_words:	
 		word_count_dict[word.lower()] = 0
-		words_f.write(word.lower()+'\n')
-	words_f.close()
+		#words_f.write(word.lower()+'\n')	
+	#print word_count_dict
 	for w in file_words:
-		word_count_dict[w.lower()] += 1  	
+		#if w in all_words:
+		word_count_dict[w] += 1  	
 	return clean_output_4_svm(word_count_dict)
 	
 #make the output of form feature:value, no zeo valued features
@@ -68,24 +72,22 @@ def get_all_words(file_name):
 	for line in lines:
 		spaces = (remove_wierd_chars(line)).split(' ')
 		for space in spaces:
-			words_in_file.add(space)
+			if space != '\n':
+				words_in_file.add(space)
 	return words_in_file
+# get a set of all words in all docs
+def get_all_words_file():
+        words_in_file = list()
+        f = open(WORDS, 'r')
+        lines = f.readlines()
+	#print lines
+        for line in lines:
+                line = line.replace('\n','')
+		words_in_file.append(line)
+		#print words_in_file
+        return words_in_file
 	
 def remove_wierd_chars(string):
-	string = string.replace('{',' ')
-        string = string.replace('}',' ')
-        string = string.replace(')',' ')
-        string = string.replace('(',' ')
-        string = string.replace('.',' ')
-        string = string.replace('!',' ')
-        string = string.replace(';',' ')
-        string = string.replace('"',' ')
-        string = string.replace('\n',' ')
-        string = string.replace('\t',' ')
-        string = string.replace(',',' ')
-        string = string.replace('  ',' ')
-        string = string.replace('  ',' ')
-	
 	string = string.replace('{',' ')
 	string = string.replace('}',' ')
 	string = string.replace(')',' ')
